4d8c3e44e59eb5a2d21f42785720753c4e79d116
jungseok-corine <nonehour@naver.com>
2023-11-23 23:00:42 +0900

    시작

---
+++ BMI_Calculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BMI_Calculator
+//
+//  Created by 오정석 on 23/11/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
