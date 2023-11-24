b0cf09522bbb3de46411c9576884e21583f0f413
jungseok-corine <nonehour@naver.com>
2023-11-24 15:21:41 +0900

    UI설정 완료

--- BMI_Calculator/ViewController.swift
+++ BMI_Calculator/ViewController.swift
@@ -9,11 +9,46 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var mainLabel: UILabel!
+    @IBOutlet weak var heightTextField: UITextField!
+    @IBOutlet weak var weightTextField: UITextField!
+    @IBOutlet weak var calculateButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        
+        makeUI()
+        
+        
+        
     }
-
-
+    
+    
+    func makeUI() {
+        //텍스트 필드 대리자 설정
+        heightTextField.delegate = self
+        weightTextField.delegate = self
+        
+        //버튼 둥글게 설정
+        calculateButton.clipsToBounds = true
+        calculateButton.layer.cornerRadius = 5
+        
+    }
+    
+    
+    
+    @IBAction func calculateButtonTapped(_ sender: UIButton) {
+        
+        
+        
+        
+    }
+    
+    
+    
 }
 
+extension ViewController: UITextFieldDelegate{
+    
+}
